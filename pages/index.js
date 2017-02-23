8263233e38f584ea8a6863c19e5e1e080b4af55c
Andrew Monks <a@monks.co>
2017-02-23 14:40:03 -0600

    chore: add state to demo

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,46 @@ import {TweenMax} from 'gsap'
 import React from 'react'
 import ReactAnimable from '../src/'
 
-export default () => {
-  return (
-    <main>
-      <ReactAnimable
-        makeTimeline={el => TweenMax.from(el, 1, {opacity: 0, x: -100})}
-      >
-        THING
-      </ReactAnimable>
-    </main>
-  )
+export default class Demo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.toggleState = this.toggleState.bind(this)
+
+    this.state = {
+      opacity: 0,
+      x: -100
+    }
+  }
+
+  toggleState () {
+    if (this.state.opacity === 0) {
+      this.setState({
+        opacity: 1,
+        x: 100
+      })
+    } else {
+      this.setState({
+        opacity: 0,
+        x: -100
+      })
+    }
+  }
+
+  componentDidMount () {
+    setInterval(this.toggleState, 2000)
+  }
+
+  render () {
+    const {opacity, x} = this.state
+    return (
+      <main>
+        <ReactAnimable
+          makeTimeline={el => TweenMax.to(el, 1, {opacity, x})}
+        >
+          THING
+        </ReactAnimable>
+      </main>
+    )
+  }
 }
 
