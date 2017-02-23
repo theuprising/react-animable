af021bf5ed2b735aa8cc09689d51edceb282d589
Andrew Monks <a@monks.co>
2017-02-23 14:24:40 -0600

    feat: add debug statements

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
+const debug = require('debug')('react-animable')
 import React from 'react'
 import GSAP from 'react-gsap-enhancer'
 
 const Animable = GSAP(
   class Animable extends React.Component {
     constructor (props) {
+      debug('constructing', props)
       super(props)
       this.makeTimeline = this.makeTimeline.bind(this)
     }
     makeTimeline (props) {
       const _makeTimeline = (props && props.makeTimeline) || this.props.makeTimeline
+      debug('making timleine', props, this.props)
       return (utils) => {
         return _makeTimeline(utils.target)
       }
     }
     componentDidMount () {
+      debug('did mount')
       this.animationController = this.addAnimation(
         this.makeTimeline()
       )
     }
     componentWillReceiveProps (nextProps) {
+      debug('receiving props')
       if (nextProps.makeTimeline !== this.props.nextTimeline) {
         this.makeTimeline(nextProps)
       }
     }
     render () {
+      debug('render')
       const {children, id, className} = this.props
       return (
         <div id={id} className={className}>{children}</div>
